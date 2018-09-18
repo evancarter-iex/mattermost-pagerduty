a43a3fc0143781cfecf6ffe8dda14965cf2ef469
Evan Carter <evan.carter@iextrading.com>
2018-09-18 09:55:20 -0400

    add ability to set port on command line

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from tornado.options import options
 from tornado.log import enable_pretty_logging
 
 from handlers.PagerDutyHandler import PagerDutyHandler
+import argparse
 import opt
 
 application = Application(
@@ -13,8 +14,18 @@ application = Application(
     ]
 )
 
-if __name__ == '__main__':
+
+def main():
     enable_pretty_logging()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=8000)
+    args = parser.parse_args()
+
     http_server = HTTPServer(application)
-    http_server.listen(options.port)
+    http_server.listen(args.port, address='0.0.0.0')
     IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
